5bd16451f6992e19b999e216927b1f6cf832ecfe
Nicolas Bédrunes <62482340+nicos-code@users.noreply.github.com>
2021-09-27 21:04:50 +0200

    Création de plusieurs unités militaires et passage de la class Villageois en anglais (Villager)

--- Unit.py
+++ Unit.py
@@ -1,22 +1,55 @@
 from Entity import *
-    
-class Villager(Unit):#herite de la classe Personnage
-    def __init__(self, x, y):
-        super().__init__(self, x, y, 50, 3)#utilise le constructeur de la classe mere
-        self.ressource = {"bois" : 0, "nourriture" : 0, "pierre" : 0, "or" : 0}#initialise les attributs specifiques a cette classe
-        self.max_ressource = 10
 
-    def nb_ressources(self):
+class Unit(Entity):
+    #une unite est une Entity qui est mobile
+    #Liste des Unit: https://ageofempires.fandom.com/wiki/Units_(Age_of_Empires) 
+    def __init__(self, x, y, health, damage):
+        super().__init__(x, y, health, damage)
+
+class Villager(Unit):#un Villageois est une Unit particuliere
+    def __init__(self, x, y, health=25, damage=3):
+        super().__init__(x, y, health, damage)#utilise le constructeur de la classe mere
+        #!!!!!!!! Faire une enumeration pour le nom des ressources
+        #avec un acces en faisant Resource.WOOD, Resource.FOOD ...
+        self.resource = {"bois" : 0, "nourriture" : 0, "pierre" : 0, "or" : 0}#initialise les attributs specifiques a cette classe
+        self.max_resource = 10
+
+    def nb_resources(self):
         nb = 0
-        for ressource in self.ressource:
-            nb += self.ressource[ressource]
+        for resource in self.resource:
+            nb += self.resource[resource]
         return nb
     
-    def is_full(self):#ne peut plus prendre de nouvelles ressources
-        return self.nb_ressources() == self.max_ressource
+    def is_full(self):#ne peut plus prendre de nouvelles resources
+        return self.nb_resources() == self.max_resource
 
-class Military(Unit):
+class Military(Unit):#un Militaire est une Unit particuliere
     def __init__(self, x, y, health, damage):
         super().__init__(self, x, y, health, damage)
 
+#Infantry
+class Clubman(Military):
+    def __init__(self, x, y, health=40, damage=3):
+        super().__init__(x, y, health, damage)
 
+class Swordsman(Military):
+    def __init__(self, x, y, health=60, damage=7):
+        super().__init__(x, y, health, damage)
+
+#Archery
+class Bowman(Military):
+    def __init__(self, x, y, health=35, damage=3):
+        super().__init__(x, y, health, damage)
+
+class ImprovedBowman(Military):
+    def __init__(self, x, y, health=40, damage=4):
+        super().__init__(x, y, health, damage)
+
+#Cavalry
+class Scout(Military):
+    def __init__(self, x, y, health=60, damage=3):
+        super().__init__(x, y, health, damage)
+
+class Cavalry(Military):
+    def __init__(self, x, y, health=150, damage=8):
+        super().__init__(x, y, health, damage)
