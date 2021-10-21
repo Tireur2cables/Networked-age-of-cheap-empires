25440024040678ff71dd2ec92d46a227417b591d
kenzo6c <kenzo.carneiro.36@gmail.com>
2021-10-21 18:08:59 +0200

    Corrected the isometric map + did some optimization

--- map/map.py
+++ map/map.py
@@ -6,26 +6,29 @@ import arcade
 CHARACTER_SCALING = 1
 TILE_SCALING = 1
 TILE_SIZE = 64
+TILE_DIST = TILE_SIZE // 2
 
-class Map() :
-	def __init__(self, view) :
+
+class Map():
+	def __init__(self, view):
 		self.view = view
 		self.generateMap()
 
-	def generateMap(self) :
+	def generateMap(self):
 		# def offset
-		#offset = 4 * self.window.width // 3
+		# offset = 4 * self.window.width // 3
 
-		for x in range(0, self.view.game.window.width, 40) :
-			for y in range(0, self.view.game.window.width, 40) :
-				isox, isoy = self.cart_to_iso(x, y)
+		for x in range(self.view.game.window.width, 0, -TILE_DIST):
+			for y in range(self.view.game.window.width, 0, -TILE_DIST):
+				isox, isoy = Map.cart_to_iso(x, y)  # Cette ligne ne recréer pas une map (testé et vérifié).
 				ground = arcade.Sprite("./map/Tiles/Tiles/ts_grass0/straight/225/0.png", TILE_SCALING)
 				ground.center_x = isox
 				ground.center_y = isoy
 				self.view.ground_list.append(ground)
 
 	# Convert cartesian coordinates to isometric
-	def cart_to_iso(self, x, y) :
+	@staticmethod  # This decorator means that the method below won't use information from the instance or the class (we don't use "self").
+	def cart_to_iso(x, y):
 		iso_x = x - y
 		iso_y = (x + y) / 2
 		return iso_x, iso_y
