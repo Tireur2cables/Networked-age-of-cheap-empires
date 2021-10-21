19ddfdb53b8302fe7973ea5484dc7a960ab2f4ab
kenzo6c <kenzo.carneiro.36@gmail.com>
2021-10-21 18:28:41 +0200

    J'ai changé la musique parce que j'en pouvais plus de la boucle de 5 secondes (au secours).

--- game.py
+++ game.py
@@ -13,19 +13,20 @@ SPRITE_SCALING_COIN = 0.2
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Age Of Cheap Empire"
-MUSIC = "./Ressources/music/logon_aoe_music3s.wav"
+MUSIC = "./Ressources/music/Marked.mp3"
+
 
 #########################################################################
 #							MAIN CLASS									#
 #########################################################################
 
-class AoCE(arcade.Window) :
+class AoCE(arcade.Window):
 
-	def __init__(self) :
+	def __init__(self):
 		""" Initializer """
 		# Call the initializer of arcade.Window
 		super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=False, fullscreen=True)
-		#arcade.set_background_color(arcade.csscolor.WHITE)
+		# arcade.set_background_color(arcade.csscolor.WHITE)
 
 		# Show the mouse cursor
 		self.set_mouse_visible(True)
@@ -39,29 +40,29 @@ class AoCE(arcade.Window) :
 		# # Variables for communications between model, view and controller.
 		# self.toDraw = []
 
-	def on_show(self) :
+	def on_show(self):
 		# Affiche le main menu
 		start_view = MainView(self.game_view)
 		start_view.setup()
 		self.show_view(start_view)
 
 	# Stop all process and exit arcade
-	def exit(self) :
+	def exit(self):
 		self.media_player.delete()
 		arcade.exit()
 
 	# Set fulllscreen or defaults : SCREEN_WIDTH x SCREEN_HEIGHT
-	def triggerFullscreen(self) :
+	def triggerFullscreen(self):
 		self.set_fullscreen(not self.fullscreen)
 
 	# Stop or play the music
-	def triggerMusic(self) :
-		if self.media_player.playing :
+	def triggerMusic(self):
+		if self.media_player.playing:
 			self.media_player.pause()
-		else :
+		else:
 			self.media_player.play()
 
-	def isPlayingMusic(self) :
+	def isPlayingMusic(self):
 		return self.media_player.playing
 
 
@@ -69,9 +70,9 @@ class AoCE(arcade.Window) :
 #							GAME VIEW									#
 #########################################################################
 
-class GameView(arcade.View) :
+class GameView(arcade.View):
 
-	def __init__(self) :
+	def __init__(self):
 		super().__init__()
 
 		# Créer l'architecture MVC
@@ -79,7 +80,7 @@ class GameView(arcade.View) :
 		self.game_view = View(self)  # Je ne sais pas comment modifier autrement la valeur de "set_mouse_visible"
 		self.game_controller = Controller(self)
 
-	def setup(self) :
+	def setup(self):
 		""" Set up the game and initialize the variables. (Re-called when we want to restart the game without exiting it)."""
 		self.game_model.setup()
 		self.game_view.setup()
@@ -100,26 +101,26 @@ class GameView(arcade.View) :
 	def on_show(self):
 		self.game_view.on_show()
 
-	def on_hide_view(self) :
+	def on_hide_view(self):
 		self.manager.disable()
 
 #########################################################################
 #							MODEL CLASS									#
 #########################################################################
 
-class Model() :
+class Model():
 
-	def __init__(self, aoce_game) :
+	def __init__(self, aoce_game):
 		""" Initializer """
 		self.game = aoce_game
-		# self.entity_list = []
+		self.entity_list = []
 
-	def setup(self) :
+	def setup(self):
 		# Set up the villager and add it to the entity_list.
-		# unit1 = Unit(Vector(50, 50), 10, 10)
-		# unit2 = Unit(Vector(100, 100), 10, 10)
-		# self.entity_list.append(unit1)
-		# self.entity_list.append(unit2)
+		unit1 = Unit(Vector(50, 50), 10, 10)
+		unit2 = Unit(Vector(100, 100), 10, 10)
+		self.entity_list.append(unit1)
+		self.entity_list.append(unit2)
 		pass
 
 #########################################################################
@@ -130,22 +131,21 @@ class Model() :
 CAMERA_MOVE_STEP = 15
 CAMERA_MOVE_EDGE = 50
 
-class View() :
+class View():
 
-	def __init__(self, aoce_game) :
+	def __init__(self, aoce_game):
 		""" Initializer """
 		self.game = aoce_game
 
 		# Variables that will hold sprite lists
-		# self.sprite_list = arcade.SpriteList()
+		self.sprite_list = arcade.SpriteList()
 
-	def setup(self) :
-		# for index, item in enumerate(self.game.game_model.entity_list):
-		# 	# coin image from kenney.nl
-		# 	self.sprite_list.append(EntitySprite(index, item, "Movements/coin_01.png", SPRITE_SCALING_COIN, center_x=item.position.x, center_y=item.position.y, hit_box_algorithm="None"))
+	def setup(self):
+		for index, item in enumerate(self.game.game_model.entity_list):
+			# coin image from kenney.nl
+			self.sprite_list.append(EntitySprite(index, item, "Movements/coin_01.png", SPRITE_SCALING_COIN, center_x=item.position.x, center_y=item.position.y, hit_box_algorithm="None"))
 		# La ligne d'au dessus créer un sprite associé au personnage et le met dans une liste. Le hit_box_algorithm à non c'est pour éviter d'utiliser une hitbox complexe, inutile pour notre projet.
 		# "Movements/coin_01.png" may cause an error depending on how the IDE is configurated (what is the root directory). I now how to fix this but haven't implemented it for now.
-		pass
 
 	def on_draw(self):
 		""" Draw everything """
@@ -157,7 +157,7 @@ class View() :
 		self.camera.move_to([self.camera_x, self.camera_y], 0.5)
 		# self.sprite_list.draw()
 
-	def on_show(self) :
+	def on_show(self):
 		""" This is run once when we switch to this view """
 		self.ground_list = arcade.SpriteList(use_spatial_hash=True)
 		self.map = Map(self)
@@ -179,7 +179,7 @@ class View() :
 
 		self.addButton()
 
-	def addButton(self) :
+	def addButton(self):
 		# def button size
 		buttonsize = self.game.window.width / 6
 
@@ -211,57 +211,57 @@ class View() :
 		elif self.mouse_y >= self.game.window.height - CAMERA_MOVE_EDGE:
 			self.camera_y += CAMERA_MOVE_STEP
 
-	def on_mouse_motion(self, x, y, dx, dy) :
+	def on_mouse_motion(self, x, y, dx, dy):
 		"""Called whenever the mouse moves."""
 		#update the coords of the mouse
 		self.mouse_x = x
 		self.mouse_y = y
 
-	# def get_sprite_list(self):
-	# 	return self.sprite_list
+	def get_sprite_list(self):
+		return self.sprite_list
 
 #########################################################################
 #							CONTROLLER CLASS							#
 #########################################################################
 
-class Controller() :
-	def __init__(self, aoce_game) :
+class Controller():
+	def __init__(self, aoce_game):
 		""" Initializer """
 		self.game = aoce_game
 
-		# # Selection (will be an EntitySprite)
-		# self.selection = []
+		# Selection (will be an EntitySprite)
+		self.selection = []
 
-	def setup(self) :
+	def setup(self):
 		pass
 
-	def on_update(self, delta_time) :
-	# 	""" Movement and game logic """
-	# 	for sprite in self.selection:
-	# 		entity = sprite.entity
-	# 		if not entity.position.isalmost(entity.aim, entity.speed):  # If it is not close to where it aims, move.
-	# 			entity.position += entity.change
-	# 			sprite.center_x, sprite.center_y = tuple(entity.position)
+	def on_update(self, delta_time):
+		""" Movement and game logic """
+		for sprite in self.selection:
+			entity = sprite.entity
+			if not entity.position.isalmost(entity.aim, entity.speed):  # If it is not close to where it aims, move.
+				entity.position += entity.change
+				sprite.center_x, sprite.center_y = tuple(entity.position)
 		pass
 
 	def on_mouse_press(self, x, y, button, key_modifiers):
-	# 	mouse_position = Vector(x, y)
-	#
-	# 	if button == arcade.MOUSE_BUTTON_RIGHT:
-	# 		for i in self.selection:
-	# 			i.entity.aim_towards(mouse_position)
-	# 			print(mouse_position)
-	#
-	# 	elif button == arcade.MOUSE_BUTTON_LEFT:
-	# 		self.selection.clear()
-	# 		villagers = arcade.get_sprites_at_point(tuple(mouse_position), self.game.game_view.get_sprite_list())
-	# 		if villagers:
-	# 			self.selection.append(villagers[0])  # ou -1, jsp encore si c'est celui qui est tout derrière ou celui qui est tout devant là.
+		mouse_position = Vector(x, y)
+
+		if button == arcade.MOUSE_BUTTON_RIGHT:
+			for i in self.selection:
+				i.entity.aim_towards(mouse_position)
+				print(mouse_position)
+
+		elif button == arcade.MOUSE_BUTTON_LEFT:
+			self.selection.clear()
+			villagers = arcade.get_sprites_at_point(tuple(mouse_position), self.game.game_view.get_sprite_list())
+			if villagers:
+				self.selection.append(villagers[0])  # ou -1, jsp encore si c'est celui qui est tout derrière ou celui qui est tout devant là.
 		pass
 
 
 # Main function to launche the game
-def main() :
+def main():
 	""" Main method """
 	game = AoCE()
 	arcade.run()
