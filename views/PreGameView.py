63ee134acddf7dd8d5df35323c497b83b006ebd6
Nicolas BATTAGLINI <nicolas.battaglini@hotmail.fr>
2021-11-03 10:40:47 +0100

    séparation des views en plusieurs fichiers
    Problem : 
    
    Solution : 
    
    Note :

---
+++ views/PreGameView.py
@@ -0,0 +1,121 @@
+# Imports
+import arcade
+from views.CustomButtons import IncreButton, SelctCivilButton, NextViewButton
+from views.FakeGameView import FakeGameView
+
+#Constants
+BACKGROUND_PREGAME = "./Ressources/img/ImageSettings.png" #A changer, c'est moche
+
+class PreGameView(arcade.View):
+	def __init__(self, main_view, nbAdv=0) :
+		super().__init__()
+		self.main_view = main_view
+		self.nbAdv = nbAdv
+
+	def setup(self) :
+		if self.nbAdv == 8 :
+			self.nbAdv = 0
+		pass
+
+	def on_show(self):
+		""" This is run once when we switch to this view """
+
+		# ajoute l'image de background
+		self.texture = arcade.load_texture(BACKGROUND_PREGAME)
+
+		# add an UIManager to handle the UI.
+		self.manager = arcade.gui.UIManager()
+		self.manager.enable()
+
+		self.setupButtons()
+		self.NumEnemButton()
+		self.launch_game()
+
+	def setupButtons(self):
+		# def button size
+		buttonsize = self.window.width / 6
+
+		# Create a vertical BoxGroup to align buttons
+		self.v_box = arcade.gui.UIBoxLayout()
+
+		# Create the buttons of incrementation
+		you_civil_button = SelctCivilButton(self.window, text="Vous : Civilisation", size=buttonsize,name="Vous")
+		self.v_box.add(you_civil_button.with_space_around(bottom=20))
+
+		name = ["Hugot", "Nico", "GuiLeDav", "Maxence", "Thomas", "Kenzo", "Nicolas"]
+
+		for i in range(self.nbAdv) :
+			adv1_civil_button = SelctCivilButton(self.window, text=name[i] + " : Civilisation", size=buttonsize,name="Hugo")
+			self.v_box.add(adv1_civil_button.with_space_around(bottom=20))
+
+		quit_button = NextViewButton(self.window, self.main_view, text="Return", width=buttonsize)
+		self.v_box.add(quit_button)
+
+		# Create a widget to hold the v_box widget, that will center the buttons
+		self.manager.add(
+			arcade.gui.UIAnchorWidget(
+				anchor_x = "left",
+				align_x = buttonsize/2,
+				anchor_y = "top",
+				align_y= -buttonsize/3,
+				child = self.v_box
+			)
+		)
+
+	#Button for numbers of ennemi, it will be at the bottom of the window and in every pregame view
+	def NumEnemButton(self):
+
+		# def button size
+		buttonsize = self.window.width / 6
+
+		# Create a vertical BoxGroup to align buttons
+		self.v_box = arcade.gui.UIBoxLayout()
+
+		# Create the button
+		num_enem_button = IncreButton(self.window, PreGameView(self.main_view, self.nbAdv+1), text="Nombre d'adversaire : " + str(self.nbAdv), size=buttonsize)
+		self.v_box.add(num_enem_button.with_space_around(bottom=20))
+
+		# Create a widget to hold the v_box widget, that will center the buttons
+		self.manager.add(
+			arcade.gui.UIAnchorWidget(
+				anchor_x = "left",
+				align_x = buttonsize,
+				anchor_y = "center_y",
+				align_y= -buttonsize,
+				child = self.v_box
+			)
+		)
+
+	#Button to start the game
+	def launch_game(self):
+		# def button size
+		buttonsize = self.window.width / 6
+
+		# Create a vertical BoxGroup to align buttons
+		self.v_box = arcade.gui.UIBoxLayout()
+
+		# Create the button
+		launch_button = NextViewButton(self.window, FakeGameView(), text="Lancer la partie", width=buttonsize*(3/2))
+		self.v_box.add(launch_button.with_space_around(bottom=20))
+
+		# Create a widget to hold the v_box widget, that will center the buttonsS
+		self.manager.add(
+			arcade.gui.UIAnchorWidget(
+				anchor_x = "right",
+				align_x = -buttonsize,
+				anchor_y = "center_y",
+				align_y= -buttonsize,
+				child = self.v_box
+			)
+		)
+
+	def on_draw(self):
+		""" Draw this view """
+
+		arcade.start_render()
+		self.texture.draw_sized(self.window.width / 2, self.window.height / 2, self.window.width, self.window.height)
+		self.manager.draw()
+
+
+	def on_hide_view(self) :
+		self.manager.disable()
---
+++ views/SettingsView.py
@@ -0,0 +1,70 @@
+# Imports
+import arcade
+from views.CustomButtons import NextViewButton, CheckboxButton
+
+# Constants
+SETTINGS_BACKGROUND = "./Ressources/img/LastImageSettings.jpg"
+
+# View des paramètres accessible via ecran d'accueil
+class SettingsView(arcade.View) :
+	""" Settings view """
+
+	def __init__(self, main_view) :
+		super().__init__()
+		self.main_view = main_view
+
+	def on_show(self):
+		""" This is run once when we switch to this view """
+
+		# ajoute l'image de background
+		self.texture = arcade.load_texture(SETTINGS_BACKGROUND)
+
+		# a UIManager to handle the UI.
+		self.manager = arcade.gui.UIManager()
+		self.manager.enable()
+
+		self.setupButtons()
+
+	def setup(self) :
+		pass
+
+	def setupButtons(self) :
+		# def sizes
+		buttonsize = self.window.width / 6
+		checkboxsize = buttonsize / 2
+
+		# Create a vertical BoxGroup to align buttons
+		self.v_box = arcade.gui.UIBoxLayout()
+
+		# Create checkboxes
+		music_button = CheckboxButton(self.window, text="Musique", size=checkboxsize, ticked=self.window.isPlayingMusic(), music=True)
+		self.v_box.add(music_button.with_space_around(bottom=20))
+
+		fullscreen_button = CheckboxButton(self.window, text="Plein écran", size=checkboxsize, ticked=self.window.fullscreen, fullscreen=True)
+		self.v_box.add(fullscreen_button.with_space_around(bottom=20))
+
+		# Create the return menu
+		retour_button = NextViewButton(self.window, self.main_view, text="Retour", width=buttonsize)
+		self.v_box.add(retour_button.with_space_around(bottom=20))
+
+		# Create a widget to hold the v_box widget, that will center the buttons
+		self.manager.add(
+			arcade.gui.UIAnchorWidget(
+				anchor_x = "center_x",
+				anchor_y = "center_y",
+				child = self.v_box
+			)
+		)
+
+	def on_draw(self):
+		""" Draw this view """
+		arcade.start_render()
+
+		self.texture.draw_sized(self.window.width / 2, self.window.height / 2, self.window.width, self.window.height)
+		arcade.draw_text("Settings Screen", self.window.width / 2, self.window.height * 5 / 6, arcade.color.WARM_BLACK, font_size=50, anchor_x="center")
+
+		self.manager.draw()
+
+
+	def on_hide_view(self) :
+		self.manager.disable()
