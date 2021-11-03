6208a5e6893d81a6ced13a5209f86c41ef5239b6
Nicolas Bédrunes <62482340+nicos-code@users.noreply.github.com>
2021-11-03 11:32:47 +0100

    player.py Création de la class Player avec qq attributs

---
+++ player.py
@@ -0,0 +1,7 @@
+from CONSTANTS import Res
+
+class Player:
+    def __init__(self, IA: bool=True, qtyFood: int=200, qtyWood: int=200, qtyGold: int=100, qtyStone: int=200) -> None:
+        self.IA = IA
+        self.resource = {Res.FOOD : qtyFood, Res.WOOD : qtyWood, Res.STONE : qtyStone, Res.GOLD : qtyGold}#utilisation de l'enumeration Resource
+
