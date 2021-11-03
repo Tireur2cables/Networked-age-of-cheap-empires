26949616997a4494f5d3c5e53388e50cbbb7ca5b
Nicolas Bédrunes <62482340+nicos-code@users.noreply.github.com>
2021-11-03 12:08:47 +0100

    player.py gestion de max_villager

--- player.py
+++ player.py
@@ -4,4 +4,10 @@ class Player:
     def __init__(self, IA: bool=True, qtyFood: int=200, qtyWood: int=200, qtyGold: int=100, qtyStone: int=200) -> None:
         self.IA = IA
         self.resource = {Res.FOOD : qtyFood, Res.WOOD : qtyWood, Res.STONE : qtyStone, Res.GOLD : qtyGold}#utilisation de l'enumeration Resource
-
+        self.max_villager = 5
+    
+    def get_max_villager(self) -> int:
+        return self.max_villager
+    
+    def set_max_villager(self, nb_max_villager: int):
+        self.max_villager = nb_max_villager
