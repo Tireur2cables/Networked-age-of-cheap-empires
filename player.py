163c3b156c12fa7f6d14039db298df0c469c5432
kenzo6c <kenzo.carneiro.36@gmail.com>
2022-01-03 11:07:51 +0100

    Simplified Player class

--- player.py
+++ player.py
@@ -3,10 +3,10 @@ from CONSTANTS import Resource as Res
 class Player:
 	def __init__(self,
 				IA: bool = True,
-				qtyFood: int = 200,
-				qtyWood: int = 200,
-				qtyGold: int = 100,
-				qtyStone: int = 200) -> None:
+				qty_food: int = 200,
+				qty_wood: int = 200,
+				qty_gold: int = 100,
+				qty_stone: int = 200) -> None:
 		"""
 		Create a player.
 
@@ -16,74 +16,34 @@ class Player:
 		self.IA = IA
 
 		# resource (dictionnary initialized with qty[Resource])
-		self.resource = {Res.FOOD : qtyFood, Res.WOOD : qtyWood, Res.GOLD : qtyGold, Res.STONE : qtyStone}
-		
+		self.resource = {Res.FOOD : qty_food, Res.WOOD : qty_wood, Res.GOLD : qty_gold, Res.STONE : qty_stone}
+
 		# unit
 		self.nb_unit = 0
 		self.max_unit = 5
-	
+
 	# unit
 	def get_nb_unit(self) -> int:
 		return self.nb_unit
-	
+
 	def set_nb_unit(self, nb_unit):
 		self.nb_unit = nb_unit
 
 	def get_max_unit(self) -> int:
 		return self.max_unit
-	
+
 	def set_max_unit(self, nb_max_unit: int):
 		self.max_unit = nb_max_unit
 
-	# resource
-	#food
-	def get_food(self) -> int:
-		return self.resource[Res.FOOD]
+	# Resource
+	def get_resource(self, resource):
+		return self.resource[resource]
 
-	def set_food(self, totalFood: int):
-		self.resource[Res.FOOD] = totalFood
+	def set_resource(self, resource, total_resource):
+		self.resource[resource] = total_resource
 
-	def add_food(self, qtyFood: int):
-		self.resource[Res.FOOD] += qtyFood
-	
-	def sub_food(self, qtyFood: int):
-		self.resource[Res.FOOD] -= qtyFood
+	def add_resource(self, resource, qty_resource):
+		self.resource[resource] += qty_resource
 
-	#wood
-	def get_wood(self) -> int:
-		return self.resource[Res.WOOD]
-
-	def set_wood(self, totalWood: int):
-		self.resource[Res.WOOD] = totalWood
-
-	def add_wood(self, qtyWood: int):
-		self.resource[Res.WOOD] += qtyWood
-	
-	def sub_wood(self, qtyWood: int):
-		self.resource[Res.WOOD] -= qtyWood
-	
-	#gold
-	def get_gold(self) -> int:
-		return self.resource[Res.GOLD]
-
-	def set_gold(self, totalGold: int):
-		self.resource[Res.GOLD] = totalGold
-
-	def add_gold(self, qtyGold: int):
-		self.resource[Res.GOLD] += qtyGold
-	
-	def sub_gold(self, qtyGold: int):
-		self.resource[Res.GOLD] -= qtyGold
-	
-	#stone
-	def get_stone(self) -> int:
-		return self.resource[Res.STONE]
-
-	def set_stone(self, totalStone: int):
-		self.resource[Res.STONE] = totalStone
-
-	def add_stone(self, qtyStone: int):
-		self.resource[Res.STONE] += qtyStone
-	
-	def sub_stone(self, qtyStone: int):
-		self.resource[Res.STONE] -= qtyStone
+	def sub_resource(self, resource, qty_resource):
+		self.resource[resource] -= qty_resource
