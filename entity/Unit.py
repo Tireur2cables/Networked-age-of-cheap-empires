2efc0ddaef40563ea32558c7d65c244767ad2e7e
Nicolas BATTAGLINI <nicolas.battaglini@hotmail.fr>
2022-01-29 17:53:50 +0100

    tab
    Problem : 
    
    Solution : 
    
    Note :

--- entity/Unit.py
+++ entity/Unit.py
@@ -264,6 +264,12 @@ class BigDaddy(Military):
 	creation_cost = {Res.FOOD : 0, Res.WOOD : 0, Res.GOLD : 0, Res.STONE : 0}
 	creation_time = 0
 	def __init__(self, iso_position, health=-1):
-		super().__init__(iso_position, sprite_data=SpriteData("Ressources/img/units/bigdaddy.png", \
-			y_offset=90//2), speed=1.6, health=health, max_health=500, damage=600, rate_fire=3, pierce_armor=10, \
-				line_sight=17)
+		super().__init__(iso_position,
+		sprite_data=SpriteData("Ressources/img/units/bigdaddy.png", y_offset=90//2),
+		speed=1.6,
+		health=health,
+		max_health=500,
+		damage=600,
+		rate_fire=3,
+		pierce_armor=10,
+		line_sight=17)
