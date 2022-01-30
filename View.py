b45e3b586c172b9545a64e8019aa47f765e2d7a1
kenzo6c <kenzo.carneiro.36@gmail.com>
2022-01-30 03:14:56 +0100

    documented weird graphical bug

--- View.py
+++ View.py
@@ -625,7 +625,7 @@ class View():
 
 			for s in selected_list["player"] :
 				if isinstance(s, Entity) : # add entity info
-					titre = s.get_name().capitalize() + ("" if s.faction == "None" else " [" + s.faction + "] ")
+					titre = s.get_name().capitalize() + ("" if s.faction == "None" else " [" + s.faction + "] ") # There is a weird graphic bug. Replacing this line by : title = "Wood" also produces the bug so it's likely not due to our code...
 					entity_box_stat = arcade.gui.UITextArea(text=titre, width=width / 3, height=height)
 					self.v_box4.add(entity_box_stat.with_space_around(0, 0, 0, 0, arcade.color.DARK_JUNGLE_GREEN))
 
