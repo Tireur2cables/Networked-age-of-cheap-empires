81b179514b77c19a4a6ff4287c1e0bb572862807
Nicolas BATTAGLINI <nicolas.battaglini@hotmail.fr>
2022-03-11 15:08:08 +0100

    main.py reçoit les descripteur du tube anonyme
    Problem : 
    
    Solution : 
    
    Note :

--- lanceur.c
+++ lanceur.c
@@ -3,23 +3,62 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+#define ERROR -1
+
+int nbdigit(int);
 void error(char *);
 
 int main(int argc, char const *argv[]) {
+
+	int fd_py_to_c[2];
+	int retour = pipe(fd_py_to_c);
+	if (retour == ERROR) error("Erreur lors de la création du tube");
+
+	int fd_c_to_py[2];
+	retour = pipe(fd_c_to_py);
+	if (retour == ERROR) error("Erreur lors de la création du tube");
+	// fd[0] // lecture
+	// fd[1] // ecriture
 	int pid = fork();
 	switch (pid) {
-		case -1 : error("Erreur lors du fork");
+		case ERROR : error("Erreur lors du fork");
 		case 0 : {
+			close(fd_py_to_c[0]);
+			close(fd_c_to_py[1]);
+
 			char *cmd = "python3";
 			char *launchfile = "main.py";
-			char *tab[3] = { cmd, launchfile, NULL };
+
+			int len = nbdigit(fd_py_to_c[1]);
+			char arg1[len + 1];
+			sprintf(arg1, "%d", fd_py_to_c[1]);
+
+			len = nbdigit(fd_c_to_py[0]);
+			char arg2[len + 1];
+			sprintf(arg2, "%d", fd_c_to_py[0]);
+
+			char *tab[5] = { cmd, launchfile, arg1, arg2, NULL };
 			execvp(cmd, tab);
 		}
-		default : printf("Je suis le père (programme C en attente)\n");
+		default : {
+			close(fd_py_to_c[1]);
+			close(fd_c_to_py[0]);
+			printf("Je suis le père (programme C en attente)\n");
+		}
 	}
 
 	return 0;
 }
+
+int nbdigit(int n) {
+	int res = 1;
+	while (n>9) {
+		res++;
+		n/=10;
+	}
+	return res;
+}
+
 
 void error(char *mess) {
 	perror(mess);
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Import the module you want to launch
 # Example : import objects.Entity to launch Entity.py locatated in the objects directory.
 import game
+import sys
 
 if __name__ == "__main__":  # Python syntax that means "if you are launching from this file, run main()", useful if this file is going to be imported.
+	print(str(sys.argv))
 	game.main()
