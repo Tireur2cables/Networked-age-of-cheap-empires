afb562618c743cfde1eb67641b1bd70d665b046f
Tireur2cables <nicolas.battaglini@hotmail.fr>
2022-06-11 16:07:14 +0200

    entrez ip
    Problem : 
    
    Solution : 
    
    Note :

--- views/MultiplayerJoinView.py
+++ views/MultiplayerJoinView.py
@@ -65,7 +65,7 @@ class MultiplayerJoinView(arcade.View):
 		buttonsize = self.window.width / 5
 		join_game = InputIP(self.window, self.window.width/3,
 		 	self.window.height/1.65,
-			text="90.92.185.45",
+			text="Entrez un IP",
 			font_name="calibri",
 			font_size=40,
 			width=buttonsize,
